25517902e2e33aed2095e26b484f0b8140c97643
jaleeleh <jaleeleha@gmail.com>
2021-01-27 14:07:08 +0200

    changes

---
+++ add.js
@@ -0,0 +1,18 @@
+var userName = prompt('whats you name?');
+alert('welcome '+userName+'in our website');
+
+
+var myCar = prompt('whats your car brand?')
+document.write('<h2>' +'my car is'+ myCar +'</h2>');
+
+
+
+
+var message = prompt('do you have credit card?');
+if(message=='yes'){
+    alert('you can use our services');
+
+}else
+{alert('you can not use our services');
+
+}
