de993bb23d00927c7b9ae5c8ffa9486dc3056d70
jaleeleh <jaleeleha@gmail.com>
2021-01-28 14:43:28 +0200

    changes

--- add.js
+++ add.js
@@ -1,4 +1,4 @@
-var userName = prompt('whats you name?');
+ /*var userName = prompt('whats you name?');
 alert('welcome '+userName+'in our website');
 
 
@@ -15,4 +15,32 @@ if(message=='yes'){
 }else
 {alert('you can not use our services');
 
+} */
+
+var brand = prompt('whats carbrand would you like to rent: toyota or hyundai or ford?');
+var theCar='';
+
+
+while(brand !=='toyota' && brand!=='ford' && brand!=='huyndai'){
+    brand=prompt('please you have to write one of them  ')
 }
+if(brand=='toyota'){
+    theCar= '<img src="images/toyota.jpg" />';
+
+}
+else if(brand=='hyundai'){
+    theCar= '<img src="images/hyundai.jpg" />';
+}else if(brand=='ford'){
+    theCar='<img src="images/ford.jpg" />'
+}
+
+
+allCars='';
+allCars=theCar;
+var numCar=prompt('how many cars would you like to rent');
+for(var i = 1 ; i<numCar ; i++){
+    
+    allCars =allCars+ theCar;
+}
+
+document.write('here the cars that you want to rent'+'<br>'+allCars);
