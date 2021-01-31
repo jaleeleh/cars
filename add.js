854bbc169418ea017d50fef48e451b781c37789c
jaleeleh <jaleeleha@gmail.com>
2021-01-31 16:36:01 +0200

    fixing

--- add.js
+++ add.js
@@ -1,4 +1,4 @@
- /*var userName = prompt('whats you name?');
+var userName = prompt('whats you name?');
 alert('welcome '+userName+'in our website');
 
 
@@ -6,16 +6,18 @@ var myCar = prompt('whats your car brand?')
 document.write('<h2>' +'my car is'+ myCar +'</h2>');
 
 
-
+function checkUp(){
 
 var message = prompt('do you have credit card?');
 if(message=='yes'){
-    alert('you can use our services');
+     return alert('you can use our services');
 
 }else
-{alert('you can not use our services');
+{ return alert('you can not use our services');
 
-} */
+} 
+}
+function showingImage() {
 
 var brand = prompt('whats carbrand would you like to rent: toyota or hyundai or ford?');
 
@@ -25,13 +27,15 @@ while(brand !=='toyota' && brand!=='ford' && brand!=='huyndai'){
  
     brand=prompt('please you have to write one of them  ')
 }
+
+
 var theCar='';
-if(brand=='toyota'){
+if(brand==='toyota'){
     theCar= '<img src="images/toyota.jpg" />';
 }
-else if(brand=='hyundai'){
+else if(brand==='hyundai'){
     theCar= '<img src="images/hyundai.jpg" />';
-}else if(brand=='ford'){
+}else if(brand==='ford'){
     theCar='<img src="images/ford.jpg" />'
 }
 
@@ -44,4 +48,5 @@ for(var i = 1 ; i<numCar ; i++){
     allCars =allCars+ theCar;
 }
 
-document.write('here the cars that you want to rent'+'<br>'+allCars);
+ return document.write('here the cars that you want to rent'+'<br>'+allCars);
+}
